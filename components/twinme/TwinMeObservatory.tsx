12f42393a41d566fc5d92c000a65b2884d1f07af
Neo <Neoschie.roberts@yahoo.com>
2026-06-24 16:49:36 -0700

    Create TwinMe observatory component shell

---
+++ components/twinme/TwinMeObservatory.tsx
@@ -0,0 +1,32 @@
+type Props = {
+
+theme:any;
+
+awareness:any;
+
+twinMeObservation:string;
+
+};
+
+
+export function TwinMeObservatory({
+
+theme,
+
+awareness,
+
+twinMeObservation
+
+}:Props){
+
+return(
+
+<>
+
+{/* Observatory goes here */}
+
+</>
+
+);
+
+}
